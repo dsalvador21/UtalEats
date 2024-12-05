faf5bb592a5a45270ef886917b6bc43f072b9686
tho <tlizana2002@gmail.com>
2024-12-04 22:01:40 -0300

    Ratings 2.0 =)

--- ratings-svc/src/main/java/com/icc/ratings_svc/RatingsSvcApplication.java
+++ ratings-svc/src/main/java/com/icc/ratings_svc/RatingsSvcApplication.java
@@ -1,4 +1,4 @@
-package src.main.java.com.icc.ratings_svc;
+package com.icc.ratings_svc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
